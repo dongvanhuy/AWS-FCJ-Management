b9b786c09e41fdfd542019cce37add66653cd351
AWS-First-Cloud-Journey <awsfcj@gmail.com>
2023-09-08 23:26:06 +0700

    update

--- install_mysql.sh
+++ install_mysql.sh
@@ -32,7 +32,7 @@ mysql -V
 sudo mysql_secure_installation
 
 # Create or update the .env file with database information
-echo "DB_HOST=$DB_HOST" > .env
+echo "DB_HOST=$DB_HOST" >> .env
 echo "DB_NAME=$DB_NAME" >> .env
 echo "DB_USER=$DB_USER" >> .env
 echo "DB_PASS=$DB_PASS" >> .env
