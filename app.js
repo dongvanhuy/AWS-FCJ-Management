1a0bce315786e19a5f16f9588bfb039fd3f578de
Huy Dong <huydong@Huys-MacBook-Pro.local>
2024-09-17 14:43:37 +0700

    update connect daatbase

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,28 +16,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Templeting engines
-app.engine('hbs',exphbs( {extname: '.hbs' }));
-app.set('view engine', 'hbs');
+app.engine("hbs", exphbs({ extname: ".hbs" }));
+app.set("view engine", "hbs");
 
 // Connection pool
 const pool = mysql.createPool({
-    connectionLimit : 100,
-    host            : process.env.DB_HOST,
-    user            : process.env.DB_USER,
-    password        : process.env.DB_PASS,
-    database        : process.env.DB_NAME,
+  connectionLimit: 100,
+  host: "fcj-management-db-instance.cx4ccmg4mypa.ap-southeast-1.rds.amazonaws.com",
+  user: "usermgt",
+  password: "admin",
+  database: "123Vodanhphai",
+  port: 3306,
 });
 
 // connect to DB
 pool.getConnection((err, connection) => {
-    if(err) throw err; //not connected!!
-    console.log('connected as ID ' + connection.threadId);
+  if (err) throw err; //not connected!!
+  console.log("connected as ID " + connection.threadId);
 });
 
-const routes = require('./server/routes/user');
-app.use('/', routes);
+const routes = require("./server/routes/user");
+app.use("/", routes);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
